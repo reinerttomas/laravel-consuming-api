3444c646f001a5f9d9744cd9d837a793c65c2bee
Tomáš Reinert <reinerttomas@gmail.com>
2024-08-12 09:10:31 +0200

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+final class User extends Authenticatable
 {
     /**
      * @phpstan-use HasFactory<\Database\Factories\UserFactory>
