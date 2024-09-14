4322627bddc548d9b581f4f365d410c069f23404
Tomáš Reinert <reinerttomas@gmail.com>
2024-09-14 21:55:28 +0200

    caching api response

--- app/Http/Integrations/GitHub/Requests/GetAuthUserRepos.php
+++ app/Http/Integrations/GitHub/Requests/GetAuthUserRepos.php
@@ -7,13 +7,20 @@ namespace App\Http\Integrations\GitHub\Requests;
 use App\DataTransferObjects\GitHub\RepoData;
 use App\Http\Integrations\GitHub\Transformers\RepoDataTransformer;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Saloon\CachePlugin\Contracts\Cacheable;
+use Saloon\CachePlugin\Contracts\Driver;
+use Saloon\CachePlugin\Drivers\LaravelCacheDriver;
+use Saloon\CachePlugin\Traits\HasCaching;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
 use Saloon\Http\Response;
 use Saloon\PaginationPlugin\Contracts\Paginatable;
 
-final class GetAuthUserRepos extends Request implements Paginatable
+final class GetAuthUserRepos extends Request implements Cacheable, Paginatable
 {
+    use HasCaching;
+
     protected Method $method = Method::GET;
 
     public function resolveEndpoint(): string
@@ -28,5 +35,15 @@ final class GetAuthUserRepos extends Request implements Paginatable
     {
         return $response->collect()
             ->map(fn (array $data): RepoData => RepoDataTransformer::make()->toDto($data));
+    }
+
+    public function resolveCacheDriver(): Driver
+    {
+        return new LaravelCacheDriver(Cache::store(config('cache.default')));
+    }
+
+    public function cacheExpiryInSeconds(): int
+    {
+        return 60; // 1 minute
     }
 }
