73ec3d3ef9338e1c0d7ce849b4589ef6f621abea
Tomáš Reinert <reinerttomas@gmail.com>
2024-09-15 14:43:14 +0200

    test throw exception when creating repo

--- tests/Feature/Services/GitHub/CreateRepoTest.php
+++ tests/Feature/Services/GitHub/CreateRepoTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\DataTransferObjects\GitHub\CreateRepoData;
 use App\DataTransferObjects\GitHub\RepoData;
+use App\Exceptions\Integrations\GitHub\GitHubException;
 use App\Http\Integrations\GitHub\Requests\CreateRepo;
 use App\Services\GitHub\GitHubService;
 use Carbon\CarbonInterface;
@@ -59,3 +60,31 @@ it('can create repo in github', function (array $data) {
         'created_at' => fake()->iso8601(),
     ],
 ]);
+
+it('throws exception if user cannot create the repo', function (array $data) {
+    // Arrange
+    Saloon::fake([
+        'user/repos' => MockResponse::make($data, 422),
+    ]);
+
+    $repoData = new CreateRepoData(
+        name: fake()->name(),
+        description: fake()->sentence(),
+        isPrivate: fake()->boolean(),
+    );
+
+    // Act
+    (new GitHubService('token'))->createRepo($repoData);
+})->with([
+    fn (): array => [
+        'message' => 'Repository creation failed.',
+        'errors' => [
+            [
+                'resource' => 'Repository',
+                'code' => 'custom',
+                'field' => 'name',
+                'message' => 'name already exists on this account',
+            ],
+        ],
+    ],
+])->throws(GitHubException::class);
